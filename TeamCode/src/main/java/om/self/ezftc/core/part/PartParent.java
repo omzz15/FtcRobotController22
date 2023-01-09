dfebae672ad3405b7af8369afa51c067beecbf72
Tom Kroon <soccer@kroon.ws>
2023-01-08 22:10:12 -0500

    12/8 After meeting rolled forward to new event manager

--- TeamCode/src/main/java/om/self/ezftc/core/part/PartParent.java
+++ TeamCode/src/main/java/om/self/ezftc/core/part/PartParent.java
@@ -2,7 +2,7 @@ package om.self.ezftc.core.part;
 
 import om.self.beans.core.BeanManager;
 import om.self.task.core.Group;
-import om.self.task.core.EventManager;
+import om.self.task.event.EventManager;
 
 public interface PartParent {
     String getName();
