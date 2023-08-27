ab2d78165e3e00a4ed6d5ddb30ec0df5db1678c2
omzz15 <ompatel1359@gmail.com>
2023-08-27 18:30:17 -0400

    finished BulkRead prebuilt

--- TeamCode/src/main/java/om/self/ezftc/prebuilt/BulkRead.java
+++ TeamCode/src/main/java/om/self/ezftc/prebuilt/BulkRead.java
@@ -7,14 +7,34 @@ import org.apache.commons.lang3.ObjectUtils;
 import java.util.List;
 
 import om.self.ezftc.core.Robot;
+import om.self.ezftc.core.part.LoopedPart;
+import om.self.ezftc.core.part.Part;
 
-public class BulkRead extends LoopedPartImpl<Robot, ObjectUtils.Null, ObjectUtils.Null> {
+/**
+ * A class that reads bulk data from all hubs at the end of each loop to speedup reading data. Use this if you are reading a lot of data and loop times are slow.
+ * <br>
+ * Note: This part will automatically start bulk reading. Noting needs to be done other than adding it to the robot.
+ */
+public class BulkRead extends Part<Robot, ObjectUtils.Null, ObjectUtils.Null> implements LoopedPart {
+    /**
+     * A list of all hubs to bulk read from
+     */
     private List<LynxModule> allHubs;
 
+    /**
+     * Creates a bulk read part for the given parent
+     * @param parent the parent robot
+     */
     public BulkRead(Robot parent) {
         super(parent, "bulk read", parent.endTaskManager);
+        constructLoop();
     }
 
+    /**
+     * reads all the data from the hubs
+     * <br>
+     * IMPORTANT: This should not be called manually.
+     */
     @Override
     public void onRun() {
         for (LynxModule hub : allHubs) {
@@ -27,12 +47,22 @@ public class BulkRead extends LoopedPartImpl<Robot, ObjectUtils.Null, ObjectUtil
 
     }
 
+    /**
+     * sets the bulk caching mode to manual for all hubs
+     * <br>
+     * IMPORTANT: This should not be called manually.
+     */
     @Override
     public void onInit() {
         allHubs = parent.opMode.hardwareMap.getAll(LynxModule.class);
         for (LynxModule hub : allHubs) {
             hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
         }
+    }
+
+    @Override
+    public void onInitialStart() {
+
     }
 
     @Override
